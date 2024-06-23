5a274a9c0bc1a33238439e2e095c9d942ad509e8
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-06-23 16:39:09 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; // it tells it we use the code of newer version of js.
+
+// alert(3 + 3); // we are using node js not browser.
+
+console.log(3+3);
+
+console.log("Virat Kohli");
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null
+//undefined
+//symbol
+//object
+
+let name = "Asim Ghafoor";
+let fatherName = "Ghafoor Ahmad";
+let age = 24;
+let city = "Lahore";
+let id = null;
+let number = undefined;
+let country;
+
+console.log(typeof name);
+console.log(typeof fatherName);
+console.log(typeof age);
+console.log(typeof city);
+console.log(typeof id);
+console.log(typeof number);
+console.log(typeof country);
+
+// lecture 5 is completed.
