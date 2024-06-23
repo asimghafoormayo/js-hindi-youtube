fbaabbdcb626b76859d9a08c29ca875ae7a0fb86
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-06-23 20:11:43 +0000

    ConversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,82 @@
+
+// let score = 30;
+
+// console.log(typeof(score));
+
+// let newScore = Number(score);
+// let newScoreTwo = String(score);
+// let newScoreThree = Boolean(score);
+
+// console.log(typeof(newScore));
+// console.log(typeof(newScoreTwo));
+// console.log(typeof(newScoreThree));
+
+// let newValue = "33";
+
+// console.log(typeof(newValue));
+
+// let newValueTwo = Number(newValue);
+
+// console.log(typeof(newValueTwo));
+// console.log(newValueTwo);
+
+// let newValueFour = "3333ac";
+
+// let newValueFive = Number(newValueFour);
+
+// console.log(newValueFive);
+// console.log(typeof(newValueFive));
+
+// let newValueSix = true;
+
+// let newValueSeven = Number(newValueSix);
+
+// console.log(newValueSeven);
+// console.log(typeof(newValueSeven));
+
+// let newValueEight = null;
+
+// let newValueNine = Number(newValueEight);
+
+// console.log(newValueNine);
+// console.log(typeof(newValueNine));
+
+// let newValueTen = undefined;
+
+// let newValueEleven = Number(newValueTen);
+
+// console.log(newValueEleven);
+// console.log(typeof(newValueEleven));
+
+// converting into Number
+// 33 => 33
+// "33" => 33
+// "33ac" => NaN
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+
+// Conversion into Boolean
+
+let isLoggedIn = "";
+let valueOfLoggedIn = Boolean(isLoggedIn);
+console.log(valueOfLoggedIn);
+console.log(typeof(valueOfLoggedIn));
+
+
+// 1 => true
+// 0 => false
+// "Asim" => true
+// "" => false
+
+// Conversion into String
+
+let newNumber = 50;
+
+let numberIntoString = String(newNumber);
+
+console.log(numberIntoString);
+console.log(typeof(numberIntoString));
+
+// 50 => 50
