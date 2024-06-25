afbe2fa6040c619b921e3b6097d67d1d59855f83
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-06-25 13:57:39 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2!=1);
+// console.log(2==2);
+
+// // whenever you do comparison , keep in mind that your data type should be same of both values, otherwise result will be unpredictable.
+
+// console.log("1">=2);
+// console.log("01"<2);
+
+// console.log(null>0); 
+// console.log(null<0);
+// console.log(null>=0); // in comparison null data type converts into number zero, that's why in this line it is saying true.
+
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined==0);
+
+//Strict Check === it will check values as well as data types.
+
+console.log("2"===2);
+console.log("2"==="2");
