00ac991e10233fc94324bef25c0429a614b6d0ce
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-06-26 19:46:47 +0000

    array

---
+++ 02_basics/array.js
@@ -0,0 +1,56 @@
+// array
+
+// let myArr = [0,1,2,3,4,5];
+
+// console.log(myArr[0]);
+// console.log(myArr[2]);
+
+// let newArr = new Array(1,2,3,4,5);
+// console.log(newArr[2]);
+
+// let newArrTwo = ["Hello","World","Hitesh","India"];
+// console.log(newArrTwo);
+// console.log(newArrTwo.length);
+
+// array methods
+
+// let newArr = [1,2,3,4,5];
+
+// newArr.push(6);
+// newArr.push(7);
+// newArr.pop();
+// newArr.unshift(9);
+// newArr.shift();
+// newArr.shift();
+
+// console.log(newArr);
+
+// console.log(newArr.includes(9));
+// console.log(newArr.includes(2));
+// console.log(newArr.indexOf(3));
+
+// let myArr = [1,2,3,4,5,6,7,8];
+
+// let newArr = myArr.join();
+
+// console.log(myArr);
+// console.log(typeof (myArr));
+
+// console.log(newArr);
+// console.log(typeof (newArr));
+
+// Slice and Splice method
+
+let newArr = [1,2,3,4,5];
+
+console.log(newArr);
+
+console.log(newArr.slice(1,3));
+console.log(newArr);
+
+// slice is slice the array from start point to one less point of end and it will not change the original array.
+
+console.log(newArr.splice(1,3));
+console.log(newArr);
+
+// splice is splice the array from start point to exact point of end and it will change the original array.
