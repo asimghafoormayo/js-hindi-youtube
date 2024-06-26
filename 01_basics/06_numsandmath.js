92cbbec9544cb3c56490b7426459d2588f88a8d1
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-06-26 14:33:54 +0000

    numsandmath

---
+++ 01_basics/06_numsandmath.js
@@ -0,0 +1,46 @@
+// const score = 100;
+
+// console.log(score);
+
+// let newNumber = new Number(100);
+
+// console.log(newNumber);
+
+// console.log(newNumber.toString());
+
+// console.log(newNumber.toFixed(2));
+
+// let otherNumber = 123.44456;
+
+// console.log(otherNumber.toPrecision(4));
+
+// const newOtherNumber = 1000000;
+
+// console.log(newOtherNumber.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-4))
+
+// console.log(Math.round(4.8));
+
+// console.log(Math.ceil(4.2));
+
+// console.log(Math.floor(4.9));
+
+// console.log(Math.min(2,4,6,7));
+
+// console.log(Math.max(2,4,5,7,8,9));
+
+console.log(Math.random());
+
+console.log(Math.random()*10 + 1);
+
+console.log(Math.floor(Math.random()*10 + 1));
+
+min = 10;
+max = 20;
+
+console.log(Math.floor((Math.random() * (max - min + 1) + min)));
