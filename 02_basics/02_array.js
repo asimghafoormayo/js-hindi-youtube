e30dc0595bc2cb5ec7dd7dd31f4f26a409957d55
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-06-26 20:54:33 +0000

    array

---
+++ 02_basics/02_array.js
@@ -0,0 +1,48 @@
+// array
+
+// let marvel_heros = ["superman","spiderman","thor"];
+
+// let dc_heros = ["batmen","flash"];
+
+// marvel_heros.push(dc_heros);
+// console.log(marvel_heros);
+
+// console.log(marvel_heros[3][0]);
+
+// let newHeros = marvel_heros.concat(dc_heros);
+// console.log(newHeros);
+
+// let newHeros = [...marvel_heros,...dc_heros];
+// console.log(newHeros);
+
+// let namesOne = ["Hitesh","Rajesh","Mahesh"];
+// let namesTwo = ["Asim","Virat","Dhoni"];
+// let namesThree = ["John","Jack","Stephen"];
+
+// let newNames = [...namesOne,...namesTwo,...namesThree];
+// console.log(newNames);
+
+// let newArr = [1,2,3,4,5,[2,3,4],6,[2,5,6,[2,3,4,5,6,[2,3,4,5]]],7,8,9,10];
+
+// console.log(newArr.flat(Infinity));
+
+// console.log(Array.isArray([1,2,3,4]));
+// console.log(Array.isArray("hello"));
+
+// console.log(Array.from("hitesh"));
+// console.log(Array.of("hitesh"));
+
+// let scoreOne = 100;
+// let scoreTwo = 200;
+// let scoreThree = 300;
+
+// console.log(Array.of(scoreOne,scoreTwo,scoreThree));
+
+// let name = "hitesh";
+// let score = 100;
+// let age = 32;
+// let country = "Pakistan";
+
+// console.log(Array.of(name,score,age,country));
+
+// // lecture is completed.
