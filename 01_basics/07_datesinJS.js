3fe2ee5b0cd42d3a0e0e95ac04de7111078e50e4
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-06-26 18:07:11 +0000

    datesinJs

---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,53 @@
+// Dates
+
+// let myDate = new Date();
+// console.log(myDate);
+
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof (myDate));
+
+// let newDateCreated = new Date(2023, 0, 23);
+// console.log(newDateCreated);
+// console.log(newDateCreated.toDateString());
+
+// let newDateCreatedTwo = new Date(2023, 0, 23, 5, 3);
+// console.log(newDateCreatedTwo.toLocaleString());
+
+// let newDateCreatedThree = new Date("2023-02-10");
+// console.log(newDateCreatedThree.toLocaleString());
+
+// let newDateCreatedFour = new Date("04-06-2024");
+// console.log(newDateCreatedFour.toLocaleString());
+
+// let newDateCreated = new Date("04-06-2024");
+
+// let timeStamp = Date.now();
+
+// console.log(timeStamp);
+
+// console.log(newDateCreated.getTime());
+
+// console.log((Math.floor(Date.now()/1000)));
+
+// let newDateCreated = new Date();
+// console.log(newDateCreated.getDate());
+// console.log(newDateCreated.getDay());
+// console.log(newDateCreated.getFullYear());
+
+// let newDateCreated = new Date("02-04-2024");
+// console.log(newDateCreated.getDate());
+// console.log(newDateCreated.getDay());
+// console.log(newDateCreated.getFullYear());
+
+// console.log(`day is ${newDateCreated.getDay()} and month is ${newDateCreated.getMonth()} and year is ${newDateCreated.getFullYear()}`);
+
+// let newDateCreatedTwo = new Date();
+// newDateCreatedTwo.toLocaleString('default',{
+//     weekday : "long",
+// })
+
+// // lecture is completed.
