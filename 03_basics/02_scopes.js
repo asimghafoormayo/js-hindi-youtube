c98cb4b11f5131877683461efb4b41c4b18bcf95
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-06-28 20:16:44 +0000

    scope in js

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,39 @@
+// scope, block scope and global
+
+// let score = 100; // global scope it can be accessed in the block and out of the block too.
+
+// if(true)
+// {
+//     let a = 100; // block scope it will not be accessed out of this block.
+//     console.log(score);
+// }
+
+// // console.log(a);
+
+// if(true)
+// {
+//     let a = 10;
+//     const b = 20;
+//     var c = 30;
+// }
+
+// // console.log(a);
+// // console.log(b);
+// console.log(c);
+
+// let a = 10;
+
+// if(true)
+// {
+//     let a = 100;
+//     let b = 200;
+//     console.log(a);
+// }
+
+// console.log(a);
+
+// a = 500;
+
+// console.log(a);
+
+// // in node the scope is different, and in browser the scope is different.
