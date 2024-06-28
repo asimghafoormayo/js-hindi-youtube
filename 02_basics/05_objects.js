aa0a7b49d45c2d0d09f6a3b0c619c74b701717f8
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-06-28 15:26:03 +0000

    object destructuring and JSON

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,28 @@
+// destructuring of objects
+
+// const user = {
+//     name : "hitesh",
+//     email : "hitesh@gmail.com",
+//     country : "Pakistan"
+// };
+
+// console.log(user.email);
+
+// const {name : n, email : em} = user;
+
+// console.log(n);
+// console.log(em);
+
+// JSON does not have name but it declared as object.
+
+// {
+//     name : "hitesh",
+//     coursename : "js in hindi",
+//     price : "free"
+// }
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
