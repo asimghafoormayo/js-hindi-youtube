24fe1571e9608df8b742db09a5c11d27e40478b7
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-06-28 16:05:31 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,44 @@
+// function
+
+// function myName(){
+//     console.log("Hello World");
+//     console.log("Hello World");
+// }
+
+// myName();
+// myName();
+
+// function addTwoNumbers(number1, number2){  // parameters
+//     console.log(number1 + number2);
+// }
+// addTwoNumbers(10,"4"); // arguments
+
+// function addTwoNumbers(number1,number2){
+//     return (number1+number2)
+// }
+
+// const result = addTwoNumbers(10,20);
+// console.log(result);
+
+// function userLoggedIn(username){
+//     return `${username} just logged in`;
+// }
+
+// console.log(userLoggedIn("Hitesh")); // we will not pass anything in arguments , the value in the parameter will be undefined.
+
+// function userDetail(username = "sam"){
+//     return `${username} just logged in`;
+// }
+
+// console.log(userDetail("asim"));
+
+// function userLoggedIn(username){
+//     if(username === undefined)
+//     {
+//         console.log("please enter the username");
+//         return
+//     }
+//     return `${username} just logged in`;
+// }
+
+// console.log(userLoggedIn("hitesh"));
