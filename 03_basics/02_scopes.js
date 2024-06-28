4534ca56b35ed69ea43bef895ebe62cee320260f
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-06-28 20:52:18 +0000

    functions

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -37,3 +37,48 @@
 // console.log(a);
 
 // // in node the scope is different, and in browser the scope is different.
+
+// function one()
+// {
+//     let username = "hitesh";
+//     function two()
+//     {
+//         let email = "hitesh@gmail.com";
+//         console.log(username);
+//     }
+//     // console.log(email);
+//     two();
+// }
+// one();
+
+// if(true)
+// {
+//     let username = "hitesh";
+//     if(username ==="hitesh")
+//     {
+//         let website = "YouTube";
+//         console.log(username+website);
+//     }
+//     // console.log(website);
+// }
+// // console.log(username);
+
+// console.log(addOne(2));
+
+// function addOne(num)
+// {
+//   return (num+1);
+// }
+
+// // when we execute the function before declaring it , it will execute if we define function with simple way.
+
+// console.log(addTwo(2));
+
+// let addTwo = function(num)
+// {
+//     return(num+2);
+// }
+
+// // but when we define function with the variable we cannot execute the function before declaration of the function.
+
+
