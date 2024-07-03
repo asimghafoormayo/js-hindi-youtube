4a7c34fdef83a78e70bc504ac12df3a29241c145
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-07-03 18:18:49 +0000

    for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,30 @@
+// for in loop
+
+// let myObj = {
+//     js : "javascript",
+//     cpp : "c++",
+//     rb : "ruby",
+//     swift : "swift by apple"
+// };
+
+// console.log(myObj);
+
+// for (const key in myObj) {
+//    console.log(`${key} is shortcut for ${myObj[key]}`);
+// } // for in loop is working to iterate the objects.
+
+// let myArray = [1,2,3,4,5];
+
+// for (const key in myArray) {
+//     console.log(`key is ${key} and value is ${myArray[key]}`);
+// } // for in loop is working to iterate the array.
+
+// const map = new Map();
+
+// map.set("USA","United States of America");
+// map.set("UK","United Kingdom");
+
+// for (const key in map) {
+//    console.log(key);
+// } // for in loop is not working to iterate the map object.
+
