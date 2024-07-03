d16f21b9e799ae34a142bb5e6a7954a1e4451d51
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-07-03 20:59:19 +0000

    filter

---
+++ 05_iterations/six.js
@@ -0,0 +1,34 @@
+// const coding = ["javascript","php","python","ruby","java"];
+
+// let code = coding.forEach( (item) => {
+//     console.log(item);
+//     return item;
+// } )
+
+// console.log(code);
+
+// let myNums = [1,2,3,4,5,6,7,8,9,10];
+
+// let newNum = myNums.filter( (num) => num > 4 );
+// console.log(newNum);
+
+// // filter method also takes call back function and in this we can apply condition on the parameter and it will return the parameters after putting the condition on them.
+
+// let myNums = [1,2,3,4,5,6,7,8,9,10];
+
+// let newNum = myNums.filter( (num) => {
+//     return num > 4;
+// } )
+
+// console.log(newNum);
+
+
+
+// let newNums = [];
+// myNums.forEach( (num) => {
+//     if (num > 4) {
+//         newNums.push(num);
+//     }
+// } )
+
+// console.log(newNums); // we can do it with for Each too but we have to use logic and if condition here , filter method is easy for putting the condition on the parameters.
