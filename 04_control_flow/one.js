794799f544dce8c4ca8b0b18aa5a8671aa120060
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-07-03 09:07:07 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,94 @@
+// control flow
+
+// if
+
+// if (false){
+//     console.log("hello world");
+// }
+
+// console.log("hello world too");
+
+// >, <, >= , <= , == , != , === , !==
+
+// if(10>20)
+// {
+//     console.log("it is greater");
+// }
+
+// console.log("executed");
+
+// if(10>=10){
+//     console.log("it is greater than or equal to");
+// }
+
+// if(2=="3")
+// {
+//     console.log("it is equal");
+// }
+// else
+// {
+//     console.log("it is not equal");
+// }
+
+// if(2 === "2")
+// {
+//     console.log("it is equal");
+// }
+// else
+// {
+//     console.log("it is not equal");
+// }
+
+// if(10!=20)
+// {
+//     console.log("it is true");
+// }
+// else
+// {
+//     console.log("it is not true");
+// }
+
+// let score = 200;
+
+// if (score > 100) {
+//     const power = "fly";
+//     console.log(`User Power is : ${power}`);
+// }
+
+// console.log(`User Power is : ${power}`);
+
+// const balance = 1000;
+
+// if (balance > 500) console.log("your balance is enough"); // implicit scope
+
+// let score = 500;
+
+// if (score < 100) {
+//     console.log("your grade is c");
+// }
+// else if(score <= 200)
+// {
+//     console.log("your grade is b");
+// }
+// else if(score < 300)
+// {
+//     console.log("your grade is a");
+// }
+// else
+// {
+//     console.log("your grade is a");
+// }
+
+// const isLoggedIn = true;
+// const isDebitCard = true;
+
+// if (isLoggedIn && isDebitCard) {
+//     console.log("you are allowed for buy courses");
+// }
+
+// const isLoginFromGoogle = true;
+// const isLoginFromFacebook = false;
+
+// if (isLoginFromGoogle || isLoginFromFacebook || false ) {
+//     console.log("you are allowed for buy courses");
+// }
