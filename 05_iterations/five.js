a15b64edd5627088ed9924e3402206bc0b67bd32
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-07-03 19:11:17 +0000

    for Each

---
+++ 05_iterations/five.js
@@ -0,0 +1,44 @@
+
+// const coding = ["javascript","php","c++","python","java"];
+
+// coding.forEach( function (item){ // callback is a function which does not have name.
+//     console.log(item);
+// } ) // here we have used simple function.
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printMe(item)
+// {
+//     console.log(item);
+// }
+
+// coding.forEach(printMe);
+
+
+
+// coding.forEach( (item, index, arr) => {
+//     console.log(item, index, arr);
+// } )
+
+
+
+// const myCoding = [
+//     {
+//         languagename : "javascript",
+//         filename : "js"
+//     },
+//     {
+//         languagename : "java",
+//         filename : "java"
+//     },
+//     {
+//         languagename : "c++",
+//         filename : "cpp"
+//     }
+// ]
+
+// myCoding.forEach( (item) => {
+//     console.log(item.languagename);
+// } )
