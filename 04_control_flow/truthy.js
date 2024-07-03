f1dfd5ddb40ecad22bb090eae0c827319c11089b
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-07-03 10:53:33 +0000

    truthy and falsy values

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,33 @@
+// truthy and falsy values
+
+// const userEmail = NaN;
+
+// if(userEmail)
+// {
+//     console.log("got the email");
+// }
+// else
+// {
+//     console.log("not got the email");
+// }
+
+// falsy values
+
+// "", 0, -0, BigInt 0n, false, null, undefined, NaN
+
+// truthy values
+
+// "0", " ", true, 'false', [], {}, function(){}
+
+// const userArr = [];
+
+// if (userArr.length === 0) {
+//     console.log("array is empty");
+// }
+
+// const userObj = {};
+
+// if(Object.keys(userObj).length === 0)
+// {
+//     console.log("object is empty");
+// }
