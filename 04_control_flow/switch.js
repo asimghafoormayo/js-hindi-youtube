37ebf0829036e78c6c2740c492c65d67108b2c04
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-07-03 09:29:24 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,22 @@
+// switch
+
+// const month = 3;
+
+// switch (month) {
+//   case 1:
+//     console.log("january");
+//     break;
+//   case 2:
+//     console.log("feb");
+//     break;
+//   case 3:
+//     console.log("thur");
+//     break; // if we will not put break after case it will execute all the code that come afterwards it except default code.
+//   case 4:
+//     console.log("fri");
+//     break;
+
+//   default:
+//     console.log("default value is executed");
+//     break;
+// }
