947952be8056ab2f65987c78bbb056fd8bf9e005
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-07-03 17:56:00 +0000

    for of loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,40 @@
+// for of
+
+// let arr = [1,2,3,4,5];
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const greetings = "hello world";
+
+// for (const greets of greetings) {
+//     if(greets == " ")
+//     {
+//         continue;
+//     }
+//     console.log(`Each character is ${greets}`);
+// }
+
+// Maps
+
+// const map = new Map();
+
+// map.set("IN","India");
+// map.set("USA","United States of America");
+// map.set("IN","India");
+
+// console.log(map);
+
+// for (const [key, value] of map) {
+//     console.log(key, ':-' , value);
+// } // this for of loop method is working to iterate the map object.
+
+// let myObj = {
+//     city : "Mumbai",
+//     country : "India"
+// }
+
+// for (const [key,value] of myObj) {
+//     console.log(key, ':-', value);
+// } // this for of loop method is not working to iterate the objects.
