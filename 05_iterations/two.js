53fc10a837a9a6b9957b15230ff7a679467dff52
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-07-03 16:34:28 +0000

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+// while loop
+
+// let i = 0;
+// while (i<=10) {
+//     console.log(`value of i is ${i}`);
+//     i = i + 2;
+// }
+
+// let myArray = ["flash","superman","batman"];
+
+// let arr = 0;
+// while (arr<myArray.length) {
+//     console.log(`value is ${myArray[arr]}`);
+//     arr = arr + 1;
+// }
+
+// do while loop
+
+// let i = 12;
+// do {
+//     console.log(`value is ${i}`);
+//     i++;
+// } while (i<=10);
