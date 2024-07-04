c62af3c361806d78ac3c071c77f236eecfad2eb4
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-07-04 12:22:04 +0000

    map and chaining

---
+++ 05_iterations/seven.js
@@ -0,0 +1,9 @@
+// const myNums = [1,2,3,4,5,6,7,8,9,10];
+
+// // const newNum = myNums.map( (item) => item + 10 );
+
+// // console.log(newNum);
+
+// const newNum =  myNums.map( (item) => item * 10 ).map( (item) => item +1 ).filter( (item) => item > 40 ); // chaining
+
+// console.log(newNum);
