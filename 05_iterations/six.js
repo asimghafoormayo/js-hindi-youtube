e13faf40d38e19a10b0fec3f9ea141a6449af398
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-07-04 11:48:12 +0000

    filter

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -32,3 +32,39 @@
 // } )
 
 // console.log(newNums); // we can do it with for Each too but we have to use logic and if condition here , filter method is easy for putting the condition on the parameters.
+
+// let books = [
+//     {
+//         booktitle : "book one",
+//         genre : "fiction",
+//         publish : 1981,
+//         edition : 2004
+//     },
+//     {
+//         booktitle : "book two",
+//         genre : "history",
+//         publish : 2008,
+//         edition : 2004
+//     },
+//     {
+//         booktitle : "book three",
+//         genre : "fiction",
+//         publish : 2010,
+//         edition : 2012
+//     },
+//     {
+//         booktitle : "book four",
+//         genre : "history",
+//         publish : 2012,
+//         edition : 2010
+//     }
+// ];
+
+// const userBooks = books.filter( (item) => item.genre === "history" );
+
+// const userBooksTwo = books.filter( (item) => {
+//     return item.publish > 2006 && item.genre === "history" } );
+
+// console.log(userBooksTwo);
+
+// // console.log(userBooks);
