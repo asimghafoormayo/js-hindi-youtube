da2f876dadfa12a229f26542c5f69a6fa8b0aa35
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-07-04 13:03:17 +0000

    reduce

---
+++ 05_iterations/nine.js
@@ -0,0 +1,35 @@
+// reduce
+
+// const myNums = [1,2,3];
+
+// const newNum = myNums.reduce( function(acc,currval){
+//     console.log(`acc is ${acc} and currval is ${currval}`);
+//     return acc + currval;
+// }, 0);
+
+// const newNum = myNums.reduce( (acc,currval) => acc+currval,0 );
+
+// console.log(newNum);
+
+// const shoppingCart = [
+//     {
+//         coursename : "js course",
+//         courseprice : 2999
+//     },
+//     {
+//         coursename : "py course",
+//         courseprice : 999
+//     },
+//     {
+//         coursename : "mobile dev course",
+//         courseprice : 1999
+//     },
+//     {
+//         coursename : "swift course",
+//         courseprice : 12999
+//     },
+// ];
+
+// const priceToPay = shoppingCart.reduce( (acc, item) => acc + item.courseprice, 0  );
+
+// console.log(priceToPay);
