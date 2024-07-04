dd1c86823d8de929c2aaafd935c78342738a2fd5
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-07-04 18:18:01 +0000

    difference b/w nodelist , html collection

---
+++ 06_dom/one.js
@@ -0,0 +1,9 @@
+// when we will use querySelector it will select the first element and give us the element directly.
+
+// but when we will use the querySelectorAll it will select all of the elements and give us NodeList that contain these elements in its indexes. nodelist is like the array but it is not array , it does not have map method, but it have forEach method by using this forEach method we can iterate that nodelist and get its elements.
+
+// if we want to use map method for nodelist we have to first convert it to the array then we can use the map method.
+
+
+
+// when we will use document.getElementbyClassName it will select all the elements of that class name , it give us in the form of HTML Collection , it does not have map method and also forEach method, so we cannot iterate it, we have to first convert that HTML Collection into array then we can iterate it using map or forEach method.
