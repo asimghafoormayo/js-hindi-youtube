125d6ece139982175c4d4bb0a0dfac919ff63ea5
asimghafoormayo <asimghafoorhunma@gmail.com>
2024-07-06 20:12:23 +0000

    one

---
+++ 09_advance_one/one.js
@@ -0,0 +1,6 @@
+// promise and async await both are the part of the asynchronous javascript where we will doing operation not directly immediately we have to wait for its completion or failure.
+
+// for example we are getting the data from the DB of other continent,read file system and network and cryptography, its going to take time so we have to wait here we use promise or async await.
+
+// before ES6 has involved the promises in javascript , back then we use Bluebird Library for creating these then , catch and finally things for async javascript.
+
